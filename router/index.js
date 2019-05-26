bb422446fcdd5ae5605dfeea548226ccfb60def9
Neal Miran <neal.miran@gmail.com>
2019-05-26 14:12:23 -0400

    Added next parameter

--- router/index.js
+++ router/index.js
@@ -5,43 +5,43 @@ const express = require('express');
 const router = express.Router();
 
 // Register route
-router.get('/register', (req, res) => {
+router.get('/register', (req, res, next) => {
   res.sendStatus(200);
   // res.send('Register User');
 });
 
 // Login route
-router.get('/login', (req, res) => {
+router.get('/login', (req, res, next) => {
   res.sendStatus(200);
   // res.send('Login User');
 });
 
 // All Product route
-router.get('/product', (req, res) => {
+router.get('/product', (req, res, next) => {
   // res.send('All Products');
   res.sendStatus(200);
 });
 
 // Product by ID route
-router.get('/product/:id', (req, res) => {
+router.get('/product/:id', (req, res, next) => {
   // res.send('Product by id');
   res.sendStatus(200);
 });
 
 // Create Product route
-router.post('/product', (req, res) => {
+router.post('/product', (req, res, next) => {
   // res.send('Create product');
   res.sendStatus(201);
 });
 
 // Update Product route
-router.put('/product/:id', (req, res) => {
+router.put('/product/:id', (req, res, next) => {
   // res.send('Update product');
   res.sendStatus(200);
 });
 
 // Delete by Id route
-router.delete('/product/:id', (req, res) => {
+router.delete('/product/:id', (req, res, next) => {
   // res.send('Delete product');
   res.sendStatus(204);
 });
