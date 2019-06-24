021777bce4749bf3edf7cc301d0812f574b802ac
Neal Miran <neal.miran@gmail.com>
2019-06-24 08:53:42 -0400

    Update web content

--- server/router/home.js
+++ server/router/home.js
@@ -1,26 +1,14 @@
 'use strict';
 
-const db = require('../../db');
-
 async function getHomeRoute(req, res, next) {
-  try {
-    // Get all the sun shade porducts json
-    const sunshades = await db.getSunshades();
-
-    // Send status and render home view
-    res
-      .status(200)
-      .render('home', {
-        pageId: 'home',
-        title: 'Home',
-        username: req.session.username,
-        sunshades: sunshades.map(sunglasses => ({
-          ...sunglasses,
-        })),
-      });
-  } catch (error) {
-    next(error);
-  }
+  // Send status and render home view
+  res
+    .status(200)
+    .render('home', {
+      pageId: 'home',
+      title: 'Home',
+      username: req.session.username,
+    });
 }
 
 module.exports = { get: getHomeRoute };
